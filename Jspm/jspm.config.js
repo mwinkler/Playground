e9eaa29db519b4e839f2ea384d603cc2e4d26bd7
Matthias Winkler <mwinkler@gmx.ch>
2016-07-22 21:08:40 +0200

    Add bundle command

--- Jspm/jspm.config.js
+++ Jspm/jspm.config.js
@@ -5,15 +5,7 @@ SystemJS.config({
     "app/": "src/"
   },
   browserConfig: {
-    "baseURL": "/",
-    "bundles": {
-      "dist/app.js": [
-        "app/app.ts",
-        "npm:jquery@3.1.0/dist/jquery.js",
-        "npm:jquery@3.1.0.json",
-        "github:frankwallis/plugin-typescript@5.0.8.json"
-      ]
-    }
+    "baseURL": "/"
   },
   devConfig: {
     "map": {
