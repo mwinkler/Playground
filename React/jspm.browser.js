aca1d787669bcf38d81dba51df207b12755bbc53
Matthias Winkler <Matthias.Winkler@swisscom.com>
2016-11-10 10:46:47 +0100

    Update

--- React/jspm.browser.js
+++ React/jspm.browser.js
@@ -1,8 +1,8 @@
 SystemJS.config({
   baseURL: "/",
   paths: {
-    "github:*": "/jspm_packages/github/*",
-    "npm:*": "/jspm_packages/npm/*",
-    "app/": "src/"
+    "app/": "src/",
+    "github:": "jspm_packages/github/",
+    "npm:": "jspm_packages/npm/"
   }
 });
--- React/jspm.config.js
+++ React/jspm.config.js
@@ -1,4 +1,22 @@
 SystemJS.config({
+  devConfig: {
+    "map": {
+      "os": "npm:jspm-nodelibs-os@0.2.0",
+      "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.16"
+    },
+    "packages": {
+      "npm:jspm-nodelibs-os@0.2.0": {
+        "map": {
+          "os-browserify": "npm:os-browserify@0.2.1"
+        }
+      },
+      "github:frankwallis/plugin-typescript@4.0.16": {
+        "map": {
+          "typescript": "npm:typescript@1.8.10"
+        }
+      }
+    }
+  },
   transpiler: "plugin-typescript",
   typescriptOptions: {
     "module": "system",
@@ -23,23 +41,11 @@ SystemJS.config({
   ],
   map: {
     "core-decorators": "npm:core-decorators@0.11.2",
-    "os": "github:jspm/nodelibs-os@0.2.0-alpha",
-    "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.7",
-    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+    "process": "npm:jspm-nodelibs-process@0.2.0",
     "react": "npm:react@0.14.8",
     "react-dom": "npm:react-dom@0.14.8"
   },
   packages: {
-    "github:frankwallis/plugin-typescript@4.0.7": {
-      "map": {
-        "typescript": "npm:typescript@1.8.10"
-      }
-    },
-    "github:jspm/nodelibs-os@0.2.0-alpha": {
-      "map": {
-        "os-browserify": "npm:os-browserify@0.2.1"
-      }
-    },
     "npm:react@0.14.8": {
       "map": {
         "fbjs": "npm:fbjs@0.6.1"
