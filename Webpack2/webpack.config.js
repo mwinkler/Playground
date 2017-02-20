8deb09a57bad07044c1743295d2f6936bc6bf1f2
mwinkler <mwinkler@gmx.ch>
2017-02-20 16:31:20 +0100

    Add minification in build

--- Webpack2/webpack.config.js
+++ Webpack2/webpack.config.js
@@ -1,35 +1,48 @@
 const path = require('path');
+const webpack = require('webpack');
 const Clean = require('clean-webpack-plugin');
 const Copy = require('copy-webpack-plugin');
 
-const dist = path.resolve(__dirname, 'dist');
+module.exports = function(env) {
 
-module.exports = {
-    entry: './src/index.ts',
-    output: {
-        filename: 'bundle.js',
-        path: dist
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            }
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    plugins: [
-        new Clean(['dist']),
+    const dist = path.resolve(__dirname, 'dist');
+    const isProd = env === 'production';
+
+    const plugins = [
         new Copy([{ from: 'src/index.html' }])
-    ],
-    devServer: {
+    ];
+
+    if (isProd) {
+        plugins.push(new webpack.optimize.UglifyJsPlugin({
+            comments: false
+        }));
+        plugins.push(new Clean(['dist']));
+    }
+
+    return {
+        entry: './src/index.ts',
+        output: {
+            filename: 'bundle.js',
+            path: dist
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    use: [ 'style-loader', 'css-loader' ]
+                }
+            ],
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"]
+        },
+        plugins: plugins,
+        devServer: {
+        }
     }
 };
