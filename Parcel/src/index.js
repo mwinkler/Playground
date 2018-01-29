fcd1e0eea2677f49ef6df4d6ed6b1939c9875ffc
Matthias Winkler <mwinkler@gmx.ch>
2018-01-29 13:42:29 +0100

    Fix sample

--- Parcel/src/index.js
+++ Parcel/src/index.js
@@ -1,13 +1,15 @@
 
-//import data from './data.json'
-//import './style.css'
-//import * as $ from 'jquery'
-//import 'font-awesome/css/font-awesome.css'
-//import fontawesome from '@fortawesome/fontawesome'
-import '@fortawesome/fontawesome-free-webfonts/fontawesome.css'
+import data from './assets/data.json'
+import './style.css'
+import $ from 'jquery'
+import '@fortawesome/fontawesome-free-webfonts';
+import '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css';
 
 console.log('Hello');
 
-//$('body').append('<h1>Test</h1>');
+$('body').append(`<h1>Hi ${data.Name} <i class="fa fa-battery-empty"></i></h1>`);
+$('body').append('<div class="demo"></div>');
 
-//console.log(data);
+console.log(data);
+//$('body').load(view);
+
