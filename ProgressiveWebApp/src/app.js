72ddd13d4f2f5ec376c1f02f98ddd6cbcf072ae5
Matthias Winkler <mwinkler@gmx.ch>
2018-07-12 23:10:18 +0200

    Add PWA example

---
+++ ProgressiveWebApp/src/app.js
@@ -0,0 +1,22 @@
+'use strict'
+
++function() {
+
+	if (!('serviceWorker' in navigator)) {
+		alert('This Browser does not support ServiceWorkers.')
+		return;
+	}
+
+	if (navigator.serviceWorker.controller) {
+		console.info('ServiceWorker runs')
+		return;
+	}
+
+    console.info('Registering ServiceWorker ...');
+    
+	navigator.serviceWorker
+		.register('/serviceworker.js')
+		.then(() => console.info('ServiceWorker has beed registered'))
+        .catch(err => console.error('Error while register ServiceWorker', err));
+
+}()
