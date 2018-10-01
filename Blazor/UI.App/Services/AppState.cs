e4d9dbbbaea21c13de9fcd9efa8a5a196fd51bea
Matthias Winkler <mwinkler@gmx.ch>
2018-10-01 15:00:12 +0200

    Implement component and state

---
+++ Blazor/UI.App/Services/AppState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.App.Services
+{
+    public class AppState
+    {
+        public string Name { get; set; } = "Default";
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+    }
+}
--- Blazor/UI.App/Startup.cs
+++ Blazor/UI.App/Startup.cs
@@ -11,6 +11,7 @@ namespace UI.App
             // Since Blazor is running on the server, we can use an application service
             // to read the forecast data.
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<AppState>();
         }
 
         public void Configure(IBlazorApplicationBuilder app)
