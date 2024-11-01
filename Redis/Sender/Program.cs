b30d5861351de6a2c68b1eb36b40bf0f82151fac
Matthias Winkler <mwinkler@gmx.ch>
2024-11-01 17:36:59 +0100

    Redis

---
+++ Redis/Sender/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using StackExchange.Redis;
+
+var channel = new RedisChannel("test", RedisChannel.PatternMode.Literal);
+
+Task.Run(() =>
+{
+    while (true)
+    {
+        using var redis = ConnectionMultiplexer.Connect("localhost");
+        var sub = redis.GetSubscriber();
+
+        for (int i = 0; i <= 10; i++)
+        {
+            sub.Publish(channel, $"Message {i}");
+            Console.WriteLine($"Message {i} published successfully");
+            Thread.Sleep(2000);
+        }
+    }
+});
+
+
+using var redis = ConnectionMultiplexer.Connect("localhost");
+var sub = redis.GetSubscriber();
+
+sub.Subscribe(channel, (c, val) =>
+{
+    Console.WriteLine($"Message {val} recivied");
+    throw new Exception("Bäm");
+}, CommandFlags.PreferMaster);
+
+
+
+Console.ReadKey();
